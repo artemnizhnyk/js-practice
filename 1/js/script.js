c925646e91215b037832821879c859a69d4e9e4b
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-03-14 20:56:08 +0100

    work with varibles and objects

--- 1/js/script.js
+++ 1/js/script.js
@@ -5,3 +5,20 @@ const leftBorderWidth = 1;
 
 number = 6;
 console.log(number);
+
+console.log("------------")
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+// console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = ["plum.png", "orange.jpg", "apple.bmp"];
+
+console.log("------------")
+
+console.log(arr.at(0))
