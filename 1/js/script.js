5869f2b460d0101bef5d6062091974a7209524cc
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-03-15 12:08:51 +0100

    practice with some methods

--- 1/js/script.js
+++ 1/js/script.js
@@ -19,6 +19,23 @@ console.log(obj["name"]);
 
 let arr = ["plum.png", "orange.jpg", "apple.bmp"];
 
-console.log("------------")
+console.log("------------");
 
-console.log(arr.at(0))
+console.log(arr.at(0));
+
+console.log("---------------------------------");
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer = prompt("Do you have 18?", "");
+// console.log(answer);
+
+const answers = [];
+// answers[0] = prompt("What is your first name", "");
+// answers[1] = prompt("What is your last name", "");
+// answers[2] = prompt("How old are you", "");
+
+console.log(typeof answers)
+
+console.log("---------------------------------");
