4a004aaf6e4c13392b928993039a0582068f7b5b
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-03-16 14:41:25 +0100

    practice with loops

---
+++ 5loops/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let num = 50;
+
+while (num <= 55) {
+    console.log(num++);
+};
+
+console.log("-------------------");
+
+for (let i = 0; i < num; i++) {
+    console.log(i);
+};
+
+console.log("-------------------");
+
