248f9d4077d634070e6a1eb7fc23e362afe38376
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-03-17 17:46:28 +0100

    some practice with callbacks

---
+++ 8callbackfunction/js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function first() {
+    setTimeout(function (){
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`I learn ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log(`I passed this lesson`);
+}
+
+learnJS("JavaScript", function () {
+});
+
+learnJS("Java", done);
