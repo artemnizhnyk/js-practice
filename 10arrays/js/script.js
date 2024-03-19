88c21e47f7f35416eee5351fd38626553092e03c
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-03-19 22:28:04 +0100

    practice with arrays

---
+++ 10arrays/js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const arr = [13, 288, 3, 65 ,8];
+arr.sort(function (a, b) {
+    return a - b;
+});
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.push(10);
+console.log(arr);
+
+for (let number of arr) {
+    console.log(number);
+}
+
+arr.forEach(function (item, i, array) {
+    console.log(`${i}: ${item} inside of array - ${array}`);
+});
+
+const str = prompt(``,``);
+const products = str.split(`, `);
+products.sort();
+console.log(products.join('; '));
