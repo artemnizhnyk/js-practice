846dac80fce9b89214511991d5947c748e173e35
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-05-08 15:11:10 +0200

    practice with ES6 modules

---
+++ practicelessons/49es6modules/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+import * as data from "./main.js";
+import sayHi from "./main.js";
+
+console.log(data.one);
+console.log(`----------`);
+console.log(data.two);
+console.log(`----------`);
+sayHi();
