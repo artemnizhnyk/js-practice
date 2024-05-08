5a95c057eaf87cd8e2fda1b5a2a774763977c1ee
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-05-08 11:36:23 +0200

    calories calculator added in secondproject

--- secondproject/js/script.js
+++ secondproject/js/script.js
@@ -517,4 +517,71 @@ window.addEventListener(`DOMContentLoaded`, () => {
     // nextArrow.addEventListener(`click`, () => {
     //     plusSlides(+1);
     // });
+
+
+    //calculator
+    const result = document.querySelector(`.calculating__result span`);
+    let sex, height, weight, age, ratio;
+    sex = `female`;
+    ratio = 1.375;
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = `____`;
+            return;
+        }
+        if (sex === `female`) {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInfo(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        document.querySelector(parentSelector).addEventListener(`click`, (e) => {
+
+            if (e.target.getAttribute('data-ratio')) {
+                ratio = +e.target.getAttribute('data-ratio');
+            } else {
+                sex = e.target.getAttribute(`id`);
+            }
+
+            elements.forEach(e => {
+                e.classList.remove(activeClass);
+            });
+            if (e.target.classList.contains(`calculating__choose-item`)) {
+                e.target.classList.add(activeClass);
+            }
+            calcTotal();
+        });
+    }
+
+    getStaticInfo(`#gender`, `calculating__choose-item_active`);
+    getStaticInfo(`.calculating__choose_big`, `calculating__choose-item_active`);
+
+    function getDynamicInfo(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener(`input`, () => {
+            switch (input.getAttribute(`id`)) {
+                case `height`:
+                    height = +input.value;
+                    break;
+                case `weight`:
+                    weight = +input.value;
+                    break;
+                case `age`:
+                    age = +input.value;
+                    break;
+            }
+            calcTotal();
+        });
+    }
+
+    getDynamicInfo(`#height`);
+    getDynamicInfo(`#weight`);
+    getDynamicInfo(`#age`);
 });
