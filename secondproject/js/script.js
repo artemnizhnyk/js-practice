26102250d0f81643299729320a03f8e9b3e37154
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-05-09 22:13:44 +0200

    babel added to secondproject

--- secondproject/js/script.js
+++ secondproject/js/script.js
@@ -1,4 +1,6 @@
-"use strict";
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
 import tabs from './modules/tabs';
 import modal, {openModal} from './modules/modal';
 import timer from './modules/timer';
@@ -8,18 +10,18 @@ import forms from './modules/forms';
 import slider from './modules/slider';
 
 window.addEventListener(`DOMContentLoaded`, () => {
-    const modalTimerId = setTimeout(()=> openModal(`.modal`, modalTimerId), 30000);
+    const modalTimerId = setTimeout(() => openModal(`.modal`, modalTimerId), 30000);
 
     tabs(`.tabheader__item`, `.tabcontent`, `.tabheader__items`, `tabheader__item_active`);
     modal(`[data-modal]`, `.modal`, modalTimerId);
-    timer(`.timer`,`2024-06-18`);
+    timer(`.timer`, `2024-06-18`);
     cards();
     calculator();
     forms(`.form`, modalTimerId);
     slider({
         container: `.offer__slider`,
         slide: `.offer__slide`,
-        nextArrow:`.offer__slider-next`,
+        nextArrow: `.offer__slider-next`,
         prevArrow: `.offer__slider-prev`,
         totalCounter: `#total`,
         currentCounter: `#current`,
