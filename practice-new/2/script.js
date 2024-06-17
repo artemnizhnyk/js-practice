2cd108384c51eaaddaae8dc0c6166f18f7b8d97e
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-17 15:00:35 +0200

    some practice with hoisting

--- practice-new/2/script.js
+++ practice-new/2/script.js
@@ -7,7 +7,7 @@ const calcAge = (birthYear) => {
     const printAge = () => {
         console.log(`You are ${age}, born in ${birthYear}`);
 
-        if(birthYear >= 1981 && birthYear <= 1996) {
+        if (birthYear >= 1981 && birthYear <= 1996) {
             const millenial = true;
             const str = `Oh, and you're a millenial, ${firstName}`;
             console.log(str);
@@ -25,3 +25,24 @@ const calcAge = (birthYear) => {
 
 const firstName = 'Artem';
 calcAge(2001);
+
+console.log(`-------------------------`);
+
+console.log(addDecl(2, 2));
+// console.log(addExpr(2, 2));
+// console.log(addArrow(2, 2));
+
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+function deleteShoppingCart() {
+    console.log(`All products deleted!`);
+}
