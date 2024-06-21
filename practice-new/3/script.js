d280f79901544ecc5140037d4b73c7e0001f7bc2
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-21 14:53:42 +0200

    practice with logical assignment operators

--- practice-new/3/script.js
+++ practice-new/3/script.js
@@ -188,3 +188,36 @@ if (restaurant.orderPizza) {
 }
 
 restaurant.orderPizza && restaurant.orderPizza();
+
+console.log(`------------------`);
+const rest1 = {
+    name: 'Capri',
+    numGuests: 0
+};
+
+const rest2 = {
+    name: 'La Piazza',
+    owner: 'Giovanni Rossi'
+};
+//
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
+
+// rest2.numGuests ||= 10;
+// rest1.numGuests ||= 10;
+
+rest2.numGuests ??= 10;
+rest1.numGuests ??= 10;
+
+console.log(rest2);
+console.log(rest1);
+console.log(`--------------------`);
+
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+// // rest1.owner = rest1.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest2);
+console.log(rest1);
