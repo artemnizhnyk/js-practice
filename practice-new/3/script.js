93ff9d2e76fab966252bdc4a5663922748365f0f
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-23 15:39:42 +0200

    some base practice with Map

--- practice-new/3/script.js
+++ practice-new/3/script.js
@@ -300,3 +300,37 @@ console.log(uniqueStaff);
 console.log(uniqueStaffArray);
 
 console.log(`-------------------------`);
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+
+rest.set(1, 'Firenze, Italy')
+    .set(2, 'Lisbon, Portugal')
+    .set('open', 11)
+    .set('close', 23)
+    .set(true, 'We are open')
+    .set(false, 'We are closed');
+
+console.log(rest);
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+console.log(`---------------------------`);
+const time = 11;
+console.log(rest.get(time >= rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+console.log(rest.has('open'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+rest.clear();
+console.log(rest);
+
+const oneTwoArr = [1,2];
+
+rest.set(oneTwoArr, 'Test');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(oneTwoArr));
