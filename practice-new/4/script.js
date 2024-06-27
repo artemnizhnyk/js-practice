0143997cd446aa17019ff5890dfb64543ef2030e
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-27 14:44:34 +0200

    practice with passing arguments: value and reference

--- practice-new/4/script.js
+++ practice-new/4/script.js
@@ -14,3 +14,33 @@ const createBooking = (flightNum = '_', numPassengers = 0, price = 100) => {
 
 createBooking('LH123');
 console.log(bookings);
+
+console.log(`-----------------`);
+const flight = 'LH234';
+const artem = {
+    name: 'Artem Nizhnyk',
+    passport: 2321412451
+};
+
+const checkIn = (flightNum, passenger) => {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 2321412451) {
+      alert('Check in')
+  } else {
+      alert('Wrong passport')
+  }
+};
+
+checkIn(flight, artem);
+console.log(flight);
+console.log(artem);
+
+console.log(`------------`);
+const newPassport = (person) => {
+    person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(artem);
+console.log(artem);
