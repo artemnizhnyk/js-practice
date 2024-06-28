2ae1853b666702100616b9382dcdaffdd618b3f5
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-06-28 19:39:59 +0200

    call and apply methods practice

--- practice-new/4/script.js
+++ practice-new/4/script.js
@@ -71,3 +71,47 @@ const greeterHey = greet('Hey');
 greeterHey('Artem');
 
 greet('Hello')('Archie');
+
+console.log(`--------------------`);
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+
+    book(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+        this.bookings.push({flight: `${this.iataCode}${flightNum}`, name})
+    }
+};
+
+lufthansa.book(239, 'Artem Nizhnyk');
+lufthansa.book(635, 'Daniel Matwijewsky');
+console.log(lufthansa);
+
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+}
+
+const book = lufthansa.book;
+
+// book(23, 'Valik Yurchenko');
+book.call(eurowings, 23, 'Valik Yurchenko');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Vlad Ivanov');
+console.log(lufthansa);
+
+const swiss = {
+    airline: 'Swiss',
+    iataCode: 'LX',
+    bookings: [],
+};
+
+book.call(swiss, 583, 'Taya Pichkur');
+console.log(swiss);
+
+const flightData = [583, 'Eugene Khchaturov'];
+book.apply(swiss, flightData);
+console.log(swiss);
