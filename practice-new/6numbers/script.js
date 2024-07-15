1f183d82fab3bd5f86f5fc4fdc31739ab50e1421
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-07-15 17:53:46 +0200

    practice with remainder operator

--- practice-new/6numbers/script.js
+++ practice-new/6numbers/script.js
@@ -98,3 +98,22 @@ console.log((2.7777777777777).toFixed(1));
 console.log((2.7777777777777).toFixed(2));
 console.log((2.7777777777777).toFixed(3));
 console.log((2.7777777777777).toFixed(4));
+
+console.log(`-----------------`);
+console.log(5 % 2);
+console.log(4 % 2);
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
