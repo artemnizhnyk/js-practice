b9f4fadfdd1b36b985ee9d836d23c620a73df9ca
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-07-17 13:57:09 +0200

    show date and time for every transaction functionality was added, and sort functionality was suited for new functionality

--- practiceBankApp/script.js
+++ practiceBankApp/script.js
@@ -71,14 +71,30 @@ const inputClosePin = document.querySelector('.form__input--pin');
 const displayMovements = (account, sort = false) => {
     containerMovements.innerHTML = '';
 
-    const movesProcessed = sort ? account.movements.slice().sort((move1, move2) => move1 - move2) : account.movements;
+    const sortingValues = [];
+    const movesProcessed = sort ? account.movements.slice().sort((move1, move2) => {
+        const sortingValue = move1 - move2;
+        sortingValues.push(sortingValue);
+        return sortingValue;
+    }) : account.movements;
+    let counter = 0;
+    const movesDatesProcessed = account.movementsDates.slice().sort((move1, move2) => sortingValues[counter++]);
     movesProcessed.forEach((movement, i) => {
         const type = movement > 0 ? 'deposit' : 'withdrawal';
+
+        const date = new Date(movesDatesProcessed.at(i));
+        const day = `${date.getDate()}`.padStart(2, 0);
+        const month = `${date.getMonth() + 1}`.padStart(2, 0);
+        const year = date.getFullYear();
+        const hour =  `${date.getHours()}`.padStart(2, 0);
+        const minutes = `${date.getMinutes()}`.padStart(2, 0);
+        const displayDate = `${day}/${month}/${year} ${hour}:${minutes}`;
+
 
         const html = ` 
             <div class="movements__row">
                 <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-<!--            <div class="movements__date">3 days ago</div>-->
+            <div class="movements__date">${displayDate}</div>
                 <div class="movements__value">${movement.toFixed(2)}€</div>
             </div>
         `;
@@ -205,9 +221,9 @@ const now = new Date();
 const day = `${now.getDate()}`.padStart(2, 0);
 const month = `${now.getMonth() + 1}`.padStart(2, 0);
 const year = now.getFullYear();
-const hour = now.getHours();
-const minutes = now.getMinutes();
-labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minutes}`;
+const hour =  `${now.getHours()}`.padStart(2, 0);
+const minutes = `${now.getMinutes()}`.padStart(2, 0);
+labelDate.textContent = `${day}/${month}/${year} ${hour}:${minutes}`;
 
 //FAKE LOGIN
 currentAccount = account1;
