7307e60a3899affe08a81cd1e730abfb1dbcaf83
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-07-24 21:21:53 +0200

    practice with timers

--- practice-new/6numbers/script.js
+++ practice-new/6numbers/script.js
@@ -210,3 +210,20 @@ const options = {
 };
 const dateTimeFormat = new Intl.DateTimeFormat('en-PL', options);
 console.log(dateTimeFormat.format(new Date()));
+
+console.log(`------------------`);
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+    (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+    3000,
+    ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+    const now = new Date();
+    console.log(now);
+}, 1000);
